31bc3898ed1d00d93f937419e84cbc4e36a2bfe3
Simeon L <l.simeon@sentinalcoding.eu>
2022-03-06 00:35:05 +0100

    Removed some unused Classes

--- src/main/java/eu/heliosteam/heliosumlgen/javaModel/visitor/ISquenceTraverser.java
+++
@@ -1,7 +0,0 @@
-package eu.heliosteam.heliosumlgen.javaModel.visitor;
-
-import java.io.IOException;
-
-public interface ISquenceTraverser {
-	void accept(ISequenceVisitor v) throws IOException;
-}
--- src/main/java/eu/heliosteam/heliosumlgen/javaModel/visitor/IUMLTraverser.java
+++
@@ -1,8 +0,0 @@
-package eu.heliosteam.heliosumlgen.javaModel.visitor;
-
-import java.io.IOException;
-
-public interface IUMLTraverser {
-
-	void accept(IUMLVisitor v) throws IOException;
-}
